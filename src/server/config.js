140c9c8b6a7f690bbac137e4a702e757125f12eb
Anelvin <amejiaroque@gmail.com>
2019-10-14 15:21:02 +0200

    handlebars configurado

--- src/server/config.js
+++ src/server/config.js
@@ -1,5 +1,23 @@
+const path=require('path');
+const exphbsrequire('express-handlebars');
+
+
 module.exports = (app) =>{
 
+    //settings
     app.set('port', process.env.PORT || 3000);
+    app.set('views',path.join(__dirname,'views'));
+    app.engine('.hbs', exphbs({
+        defaultLayout:'main',
+        partialsDir:path.join(app.get('views'),'partials'),
+        layoutsDir:path.join(app.get('views'),('layouts')),
+        extname:'.hbs',
+        helpers:require('./helpers')
+    }));
+
+    app.set('view engine','hbs');
+    //middlewares
+
+    //routes
     return app;
 }
