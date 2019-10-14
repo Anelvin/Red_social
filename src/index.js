6b712f7d59ab98af7cd17a1dc3616333a2c3d4a6
Anelvin <amejiaroque@gmail.com>
2019-10-14 14:10:44 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express=require('express');
+
+const config =require('./server/config');
+
+//database
+require('./database');
+//starting server
+const app=config(express());
+
+app.listen(app.get('port'),()=>{
+    console.log('Server on port '+app.get('port'));
+})
