4b317e53a03a9c50d31b4ff995907a92e661d8d5
Anelvin <amejiaroque@gmail.com>
2019-10-14 15:41:06 +0200

    servidor configurado

--- src/server/config.js
+++ src/server/config.js
@@ -1,6 +1,10 @@
 const path=require('path');
-const exphbsrequire('express-handlebars');
-
+const exphbs=require('express-handlebars');
+const morgan=require('morgan');
+const multer=require('multer');
+const express=require('express');
+const routes=require('../routes/index');
+const errorHandler=require('errorhandler');
 
 module.exports = (app) =>{
 
@@ -14,10 +18,25 @@ module.exports = (app) =>{
         extname:'.hbs',
         helpers:require('./helpers')
     }));
-
     app.set('view engine','hbs');
+
     //middlewares
+    app.use(morgan('dev'));
+    app.use(multer({dest:path.join(__dirname,'../public/upload/temp')}).single('image'));
+    app.use(express.urlencoded({extended:false}));
+    app.use(express.json());
 
     //routes
+   routes(app);
+
+
+   //static files
+   app.use('/public',express.static(path.join(__dirname,'../public')));
+
+   //errorHandler
+   if('development'===app.get('env')){
+       app.use(errorHandler);
+   }
+
     return app;
 }
