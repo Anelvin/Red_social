362e8d16862bed8fdb7fc3ed3d680b3fbc3e8a68
Anelvin <amejiaroque@gmail.com>
2019-10-30 22:06:42 +0100

    Obtención de imagenes

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,7 +1,9 @@
 const controller={}
+const {Image} = require('../models/index');
 
-controller.index=(req,res)=>{
-    res.render('index');
+controller.index=async (req,res)=>{
+    const images = await Image.find().sort({timestamp:-1});
+    res.render('index',{images});
 }
 
 module.exports=controller;
